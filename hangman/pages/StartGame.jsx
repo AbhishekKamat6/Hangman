bafdba8ae4a921502b3bd561f5ae05ac7c76636e
AbhishekKamat <128158420+ohhyesbhi@users.noreply.github.com>
2025-03-23 17:11:50 +0530

    about fragment

---
+++ hangman/pages/StartGame.jsx
@@ -0,0 +1,22 @@
+function StartGame(){
+
+    // return (
+    //   <div></div>
+    //   <div></div>
+    // )  
+
+    // NOT POSSIBLE the way we are returning the JSX in above logic
+    // From JSX we need to return single parent
+    // And also we cannot wrap inside DIV or any other element because it means we are creating extran node in dom element and to avoid css clashes
+    // So we use fragments ( <>  </> )
+
+
+    return(
+        <>
+         
+        </>
+    )
+
+}
+
+export default StartGame;
