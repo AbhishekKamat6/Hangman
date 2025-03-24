abf99451ad59b5bae4bd0630682b5399c55acfac
AbhishekKamat <128158420+ohhyesbhi@users.noreply.github.com>
2025-03-24 09:02:35 +0530

    About single responsbility principle

--- hangman/components/textInputForm/TextInputForm.jsx
+++ hangman/components/textInputForm/TextInputForm.jsx
@@ -71,3 +71,23 @@ function TextInputForm(){
 
 
 export default TextInputForm;
+
+
+// In this code we are violating the single responsibility principle.
+// It says one function should only have one responsibility.
+// If you see the above logic textInputForm component is doing multiple things
+
+// 1) First responsibility is to show how the UI should look like
+// 2) Second responsibility is to handle the form submission
+// 3) Third responsibility is to handle the text input
+
+// Ideally this should not be the case because reusability of the code is lost and let's say you want to use same to same UI somewhere else and there you don't want to handle the form submission
+// then you can't use this component because it is tightly coupled with the form submission logic
+
+// So here comes a design principle is known as presentor container pattern
+
+// So what it says is visualize the component as two parts one is presentor and other is container(logical layer)
+// Peresentation have only one work where it says how the UI looks like and container have the logic of the component
+// And now IMPORTANT thing is container layer will call the presentor layer and pass the props to it and presentor layer will just show the UI
+
+// So the use of it is if you want to use the same UI somewhere else you can use the presentor layer and pass the props to it and you can use the same UI
