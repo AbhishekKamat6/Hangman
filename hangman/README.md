517496576f684289d087d5cc808801a0ae5290ff
AbhishekKamat <128158420+ohhyesbhi@users.noreply.github.com>
2025-04-03 07:01:30 +0530

    about useEffect hook

--- hangman/README.md
+++ hangman/README.md
@@ -1,2 +1,16 @@
 
 * Single responsiblity principle -: ![alt text](image-1.png) 
+
+* Component has a different different phases when it comes on UI 
+FirstPhase -: Mounting ( Component has been inserted inside DOM )
+SecondPhase -: Render or Updating  ( Our component will get updated and based on that component UI will get changed )
+ThirdPhase -: UnMounting ( Removing the component from DOM )
+
+
+I want to track these events now , So react will give us a hook
+
+The hook is called as useEffect() 
+Primary use case -: It will help you to control instructins to be executed during different lifecycle events
+Secondary use case -: In useEffect we write a code to get the data
+
+
--- hangman/components/textInputForm/TextInputFormContainer.jsx
+++ hangman/components/textInputForm/TextInputFormContainer.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import TextInputForm from "./TextInputForm";
 import { useNavigate } from "react-router-dom";
 
@@ -53,12 +53,58 @@ function TextInputFormContainer(){
         console.log(inputType,"is the type")
       }
 
+      // // useEffect is a function where it takew two params , where one is callback function and the other is an array 
+ 
+      // useEffect(() => {
+      //   // so it will be executed two time , First time when the component is attached to dom and second time when the component is updated
+      //   console.log("Component first time mounted");
+      // },[]) 
+      // // If it is empty array then it will be executed only once when the component is mounted 
+
+      // useEffect(() => {
+      //   console.log("Component first load and update");
+      // }) // Here it will be executed every time when the component is mounted and updated because we are not passing any array
+
+      // useEffect(() => {
+      //   console.log("component first load and update value change")
+      // },[value]) // Here it will be executed only when the value is changed because we are passing value in the array
+
+      // useEffect(() => {
+      //   console.log("component first load and inputType value change")
+      // },[inputType]) // Here it will be executed only when the inputType is changed because we are passing inputType in the array
+
 
       return (
         <>
         <TextInputForm handleFormSubmit={handleFormSubmit} handleTextInput={handleTextInput} handleShowHideClick={handleShowHideClick} text={text} inputType={inputType}/>
+
+        { inputType == "password" ? <Temp/> : null }
         </>
       )
+}
+
+function Temp(){
+  useEffect(() => {
+    console.log("Component first time mounted");
+
+    return () => {
+      console.log("Component unmounted");
+    }
+
+    // The function you return in useEffect is called a cleanup function. React calls this cleanup function in two cases:
+
+  // 1)  When the component unmounts → It helps clean up side effects (e.g., remove event listeners, stop API calls, clear timers).
+  // 2)  Before running the effect again (if dependencies change) → This prevents memory leaks and avoids unintended behavior.
+
+  },[])
+
+  return (
+    <>
+    <div>
+      <h1>temp</h1>
+    </div>
+    </>
+  )
 }
 
 
